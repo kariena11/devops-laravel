660b903f13788aaba2ff19261870c7bcd081b50a
kariena <karinaadelia2004@gmail.com>
2026-03-11 08:05:16 +0700

    Menambahkan file inti Laravel dan artisan

---
+++ prod/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '69c28013f0074ca4ed3639aeeec0eca4cb02bd17',
+    'name' => 'kariena/devops-laravel',
+  ),
+  'versions' => 
+  array (
+    'kariena/devops-laravel' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '69c28013f0074ca4ed3639aeeec0eca4cb02bd17',
+    ),
+  ),
+);
